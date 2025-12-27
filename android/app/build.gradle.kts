278921d9390fa8a94ed02d95aa69c8af5974f29e
outladder <batuhanozdemirpersonal@gmail.com>
2025-12-27 21:11:22 +0300

    Build dosyalari guncellendi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // Firebase, Google Maps ve diğer modern SDK'lar için minSdk 23 gerekli
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
